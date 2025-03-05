e41b4f864a5ad5488f9b3e738f217b38dcac8fe9
Thomas RICHOU <t.richou@workstation23.nsi.lan>
2025-03-05 09:48:39 +0100

    fini

--- dico.py
+++ dico.py
@@ -3,7 +3,8 @@
 import requests
 import os
 import csv
-
+import pandas as pd
+import folium
 def recupere_donnes():
     """
     Retourne les données sous forme d'une liste de dictionnaires
@@ -20,11 +21,26 @@ def recupere_donnes():
     
     donnes = []
     # ouverture du fichier précédemment créé
-    with open(local_file) as csvfile:
+    with open(local_file, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
+        colonnes_attendues = reader.fieldnames
+        nb_colonnes = len(colonnes_attendues)
         for ligne in reader:
-            donnes.append(ligne)
+            if len(ligne) != nb_colonnes:
+                print(f"Ligne invalide (mauvais nombre de colonnes) : {ligne}")
+                continue 
+            ligne_convertie = {}
+            for cle, valeur in ligne.items():
+                if cle in ['latitude', 'longitude', 'y', 'x']: 
+                    ligne_convertie[cle] = float(valeur) if valeur else 0.0
+                elif cle in ['nombre_eleves', 'identifiant', 'code_postal']: 
+                    ligne_convertie[cle] = int(valeur) if valeur else 0
+                else:  
+                    ligne_convertie[cle] = valeur
+            donnes.append(ligne_convertie)
+    
     return donnes
+
         
 etablissements = recupere_donnes()
 
@@ -61,7 +77,7 @@ def filtrer(donnes, criteres, colonnes=None):
         return resultat
 
 
-print(filtrer(etablissements, {'COMMUNE':'CHATEAULIN'}))
+#return(filtrer(etablissements, {'COMMUNE':'CHATEAULIN', 'STATUT' : 'Privé', }))
 
 
  #[['FID',                       'the_geom',                      'OBJECTID'     'INSEE_COMM', 'COMMUNE', 'CODE_ET', 'NOM_ET', 'STATUT']
@@ -69,5 +85,71 @@ print(filtrer(etablissements, {'COMMUNE':'CHATEAULIN'}))
 #['colleges_29.1', 'POINT (139542.9111092165 6796291.165203575)', '39', '29003', 'PLOUHINEC', '0330B',   'Locquéran',      'Public']
 
 
+    """_
+  
+def trier(donnees, criteres):
+    #Retourne les données triées selon criteres
+    def multi_criteres(donnees):
+        result = []
+        for c in criteres:
+            result.append(donnees[c])
+        return result
+   
+    return sorted(donnees, key=multi_criteres)
+
+etablissements = recupere_donnes()
+#print(etablissements[0:10])
+results = trier(etablissements, ['COMMUNE', 'STATUT'])
+for result in results:
+    print(result)
+  """
+
+url = "https://www.data.gouv.fr/fr/datasets/r/bf9d46b1-5430-4866-ab7d-58a4d794324d"
+colleges_morbihan = pd.read_csv(url, delimiter=';', encoding='iso 8859-15')
 
 
+
+
+local_file = "geolocalisation.csv"
+if not local_file in os.listdir():
+    url = "https://data.education.gouv.fr/explore/dataset/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B"
+    data = requests.get(url).content
+    with open(local_file, 'wb') as csvfile:
+        csvfile.write(data)
+geolocalisations = pd.read_csv(local_file, delimiter = ";")
+
+
+
+
+
+mapper = {'numero_uai':'CODE',}
+
+geolocalisations.rename(columns=mapper, inplace=True)
+
+print(geolocalisations)
+
+
+
+f =colleges_morbihan.merge(geolocalisations, on=['CODE'], how='left')
+
+colleges_morbihan = f
+pd.set_option('display.max_rows',90)
+pd.set_option('display.max_columns',20)
+print(f)
+
+m = folium.Map([47.676116620401416,-2.7710275095710846], zoom_start=12)
+
+
+
+for i in f.index :
+    p = f["position"][i]
+    if str(p) != "nan":
+        latitude , longitude = p.split(",")
+        folium.Marker(
+            location=[float(latitude), float(longitude)],
+            tooltip="Click me!",
+            popup=f["ADRESSE"][i],
+            icon=folium.Icon(color="red"),
+        ).add_to(m)
+
+m.save('test.html')
